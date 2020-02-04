362b141fdaea8243b44e969b56c906eea771a048
Dan Gilleland <dgilleland@nait.ca>
2020-02-04 11:15:55 -0700

    Primary and Foreign Keys

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -19,7 +19,9 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Students'
 /* === Create Tables === */
 CREATE TABLE Students
 (
-    StudentID       int             NOT NULL,
+    StudentID       int
+        CONSTRAINT PK_Students_StudentID
+            PRIMARY KEY             NOT NULL,
     GivenName       varchar(50)     NOT NULL,
     Surname         varchar(50)     NOT NULL,
     DateOfBirth     datetime        NOT NULL,
@@ -28,7 +30,31 @@ CREATE TABLE Students
 
 CREATE TABLE Courses
 (
-    Number          varchar(10)     NOT NULL,
-    Name            varchar(50)     NOT NULL,
+    Number          varchar(10)
+        CONSTRAINT PK_Courses_Number
+            PRIMARY KEY             NOT NULL,
+    [Name]          varchar(50)     NOT NULL,
     Credits         decimal(3, 1)   NOT NULL,
+    [Hours]         tinyint         NOT NULL,
+    Active          bit             NOT NULL,
+    Cost            money           NOT NULL
+)
+
+CREATE TABLE StudentCourses
+(
+    StudentID       int
+        CONSTRAINT FK_StudentCourses_StudentID_Students_StudentID
+            FOREIGN KEY REFERENCES Students(StudentID)
+                                    NOT NULL,
+    CourseNumber    varchar(10)
+        CONSTRAINT FK_StudentCourses_CourseNumber_Courses_Number
+            FOREIGN KEY REFERENCES Courses(Number)
+                                    NOT NULL,
+    [Year]          tinyint         NOT NULL,
+    Term            char(3)         NOT NULL,
+    FinalMark       tinyint             NULL,
+    [Status]        char(1)         NOT NULL,
+    -- Table-level constraint for composite keys
+    CONSTRAINT PK_StudentCourses_StudentID_CourseNumber
+        PRIMARY KEY (StudentID, CourseNumber)
 )
