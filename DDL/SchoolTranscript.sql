412a85f5cee844862e0c2d69d4ed8dfdf8558e95
Dan Gilleland <dgilleland@nait.ca>
2020-02-06 13:02:19 -0700

    Check constraints

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -24,8 +24,23 @@ CREATE TABLE Students
             PRIMARY KEY
         IDENTITY(20200001, 1)       NOT NULL,
     GivenName       varchar(50)     NOT NULL,
-    Surname         varchar(50)     NOT NULL,
-    DateOfBirth     datetime        NOT NULL,
+--    % is a wildcard for zero or more characters (letter, digit, or other character)
+--    _ is a wildcard for a single character (letter, digit, or other character)
+--    [] are used to represent a range or set of characters that are allowed
+    Surname         varchar(50)
+        CONSTRAINT CK_Students_Surname
+            CHECK (Surname LIKE '__%')          -- LIKE allows us to do a "pattern-match" of values
+--          CHECK (Surname LIKE '[a-z][a-z]%')  -- two letters plus any other chars
+          --                     \ 1 /\ 1 /
+          -- Positive match for 'Fred'
+          -- Positive match for 'Wu'
+          -- Negative match for 'F'
+          -- Negative match for '2udor'
+                                    NOT NULL,
+    DateOfBirth     datetime
+        CONSTRAINT CK_Students_DateOfBirth
+            CHECK (DateOfBirth < GETDATE())
+                                    NOT NULL,
     Enrolled        bit
         CONSTRAINT DF_Students_Enrolled
             DEFAULT (1)             NOT NULL
@@ -37,12 +52,23 @@ CREATE TABLE Courses
         CONSTRAINT PK_Courses_Number
             PRIMARY KEY             NOT NULL,
     [Name]          varchar(50)     NOT NULL,
-    Credits         decimal(3, 1)   NOT NULL,
-    [Hours]         tinyint         NOT NULL,
+    Credits         decimal(3, 1)
+        CONSTRAINT CK_Courses_Credits
+            CHECK (Credits > 0 AND Credits <= 6)
+                                    NOT NULL,
+    [Hours]         tinyint
+        CONSTRAINT CK_Courses_Hours
+            CHECK ([Hours] BETWEEN 15 AND 180) -- BETWEEN operator is inclusive
+--          CHECK ([Hours] >= 15 AND [Hours] <= 180)
+                                    NOT NULL,
     Active          bit
         CONSTRAINT DF_Courses_Active
             DEFAULT (1)             NOT NULL,
-    Cost            money           NOT NULL
+    Cost            money
+        CONSTRAINT Cost
+            CHECK (Cost >= 0)
+                                    NOT NULL
+                           
 )
 
 CREATE TABLE StudentCourses
