402858de82858a779e6a476998fc5bbccc4d0c36
Dan Gilleland <dgilleland@nait.ca>
2020-02-06 13:49:25 -0700

    Indexes and Alter Table

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -84,8 +84,47 @@ CREATE TABLE StudentCourses
     [Year]          tinyint         NOT NULL,
     Term            char(3)         NOT NULL,
     FinalMark       tinyint             NULL,
-    [Status]        char(1)         NOT NULL,
+    [Status]        char(1)
+        CONSTRAINT CK_StudentCourses_Status
+            CHECK ([Status] = 'E' OR
+                   [Status] = 'C' OR
+                   [Status] = 'W')
+--          CHECK ([Status] IN ('E', 'C', 'W'))
+                                    NOT NULL,
     -- Table-level constraint for composite keys
     CONSTRAINT PK_StudentCourses_StudentID_CourseNumber
-        PRIMARY KEY (StudentID, CourseNumber)
+        PRIMARY KEY (StudentID, CourseNumber),
+    -- Table-level constraint involving more than one column
+    CONSTRAINT CK_StudentCourses_FinalMark_Status
+        CHECK (([Status] = 'C' AND FinalMark IS NOT NULL)
+               OR
+               ([Status] IN ('E', 'W') AND FinalMark IS NULL))
 )
+
+
+/* ----- Indexes ------- */
+-- For all foreign keys
+CREATE NONCLUSTERED INDEX IX_StudentCourses_StudentID
+    ON StudentCourses (StudentID)
+
+CREATE NONCLUSTERED INDEX IX_StudentCourses_CourseNumber
+    ON StudentCourses (CourseNumber)
+
+-- For other columns where searching/sorting might be important
+CREATE NONCLUSTERED INDEX IX_Students_Surname
+    ON Students (Surname)
+
+/* ------- ALTER TABLE statements ---------- */
+-- 1) Add a PostalCode for the Students table
+ALTER TABLE Students
+    ADD PostalCode char(6) NULL
+    -- Adding this as a nullable column, because students already exist,
+    -- and we don't have postal codes for those students.
+GO -- I have to break the above code as a separate batch from the following
+
+-- 2) Make sure the PostalCode follows the correct pattern A#A#A#
+ALTER TABLE Students
+    ADD CONSTRAINT CK_Students_PostalCode
+        CHECK (PostalCode LIKE '[A-Z][0-9][A-Z][0-9][A-Z][0-9]')
+        --  Match for  T4R1H2 :   T    4    R    1    H    2
+GO
