5d51d914de4fa1dcd0f58e0f82657080a1cc8c6e
Dan Gilleland <dgilleland@nait.ca>
2020-02-10 11:32:54 -0700

    Odds and Ends + Query Intro

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -128,3 +128,24 @@ ALTER TABLE Students
         CHECK (PostalCode LIKE '[A-Z][0-9][A-Z][0-9][A-Z][0-9]')
         --  Match for  T4R1H2 :   T    4    R    1    H    2
 GO
+
+-- 3) Add a default constraint for the Status column of StudentCourses.
+--    Set 'E' as the default value
+ALTER TABLE StudentCourses
+    ADD CONSTRAINT DF_StudentCourses_Status
+        DEFAULT ('E') FOR [Status] -- In an Alter Table statement, the column must be specified
+                                   -- for the default value
+GO
+
+/* ----- Other Odds and Ends ----- */
+sp_help Students -- Get schema information for the Students table
+
+-- In a table, we can have some columns be "calculated" or "derived" columns
+-- where the value of the column is a calculation from other columns
+CREATE TABLE Invoice
+(
+    InvoiceId       int         NOT NULL,
+    Subtotal        money       NOT NULL,
+    GST             money       NOT NULL,
+    Total           AS Subtotal + GST     -- This is a Computed Column
+)
--- DDL/SchoolTranscript_Data.sql
+++ DDL/SchoolTranscript_Data.sql
@@ -6,6 +6,36 @@ USE SchoolTranscript
 GO
 
 INSERT INTO Students(GivenName, Surname, DateOfBirth) -- notice no Enrolled column
-VALUES ('Dan', 'Gilleland', '19720514 10:34:09 PM')
+VALUES ('Dan', 'Gilleland', '19720514 10:34:09 PM'),
+       ('Charles', 'Kuhn', '19990806 00:00:00 AM'),
+       ('Penny', 'Harrison', '19961104 00:00:00 AM'),
+       ('Arthur', 'Watson', '19980907 00:00:00 AM'),
+       ('Jackson', 'Hudson', '20020702 00:00:00 AM')
+
 
 SELECT * FROM Students
+
+INSERT INTO Courses(Number, [Name], Credits, [Hours], Cost)
+VALUES  ('DMIT-1508', 'Database Fundamentals', 3.0, 60, 750),
+        ('CPSC-1012', 'Programming Fundamentals', 3.0, 60, 750),
+        ('DMIT-1720', 'OOP Fundamentals', 4.5, 90, 850),
+        ('DMIT-2210', 'Agile Development', 4.5, 90, 850),
+        ('DMIT-1718', 'Software Testing', 4.5, 90, 850)
+
+SELECT * FROM Courses
+/*
+-- The following should fail, because of a check constraint
+INSERT INTO Students(GivenName, Surname, DateOfBirth) -- notice no Enrolled column
+VALUES ('Dan', 'G', '19720514 10:34:09 PM')
+*/
+
+SELECT  Number, [Name], Credits, [Hours]
+FROM    Courses
+WHERE   [Name] LIKE '%Fundamentals%'
+
+-- Write a query to get the first/last names of all students
+-- whose last name starts with a "G"
+
+
+-- Removing all the data from the Students table
+DELETE FROM Students
