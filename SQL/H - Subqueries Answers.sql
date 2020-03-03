355e3ed5db98ef9e35b9edca1d55f2dfe9adca77
Dan Gilleland <dgilleland@nait.ca>
2020-03-03 11:48:08 -0700

    Subquery Answers

--- SQL/H - Subqueries Answers.sql
+++ SQL/H - Subqueries Answers.sql
@@ -82,3 +82,10 @@ HAVING  COUNT(CourseId) >= ALL (SELECT COUNT(CourseId)
 
 -- 14. Which students are most active in the clubs?
 -- TODO: Student Answer Here...
+SELECT  FirstName + ' ' + LastName
+      --, COUNT(A.ClubId)
+FROM    Student AS S
+    INNER JOIN Activity AS A
+        ON S.StudentID = A.StudentID
+GROUP BY FirstName + ' ' + LastName
+HAVING  COUNT(ClubId) >= ALL (SELECT COUNT(ClubId) FROM Activity GROUP BY StudentID)
